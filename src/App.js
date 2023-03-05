e58240720b1a6354110a889c800116329f3d0792
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-04 20:02:46 -0800

    extract login form into a helper function and render conditionally

--- src/App.js
+++ src/App.js
@@ -35,31 +35,76 @@ const App = () => {
     }
   }
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h2>log in to application</h2>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <div>blog form goes here</div>
+    // <form onSubmit={addBlog}>
+    //   <h2>create new</h2>
+    //   <div>
+    //     title
+    //     <input
+    //       type="text"
+    //       value={newTitle}
+    //       name="Title"
+    //       onChange={({ target }) => setNewTitle(target.value)}
+    //     />
+    //   </div>
+    //   <div>
+    //     author
+    //     <input
+    //       type="text"
+    //       value={newAuthor}
+    //       name="Author"
+    //       onChange={({ target }) => setNewAuthor(target.value)}
+    //     />
+    //   </div>
+    //   <div>
+    //     url
+    //     <input
+    //       type="text"
+    //       value={newUrl}
+    //       name="Url"
+    //       onChange={({ target }) => setNewUrl(target.value)}
+    //     />
+    //   </div>
+    //   <button type="submit">create</button>
+    // </form>
+  )
+
   return (
     <div>
       {/* <Notification message={errorMessage} */}
-      <h2>log in to application</h2>
-      <form onSubmit={handleLogin}>
+      {user === null ?
+        loginForm() :
         <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
+          <p>{user.name} logged in</p>
+          {blogForm()}
         </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      }
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
