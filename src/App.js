73ab59ee023aa059cbd19b79c785f1ece0800388
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-04 19:50:03 -0800

    implement method to handle login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  // const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,13 +16,28 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+      // setErrorMessage('wrong credentials')
+      // setTimeout(() => {
+      //   setErrorMessage(null)
+      // }, 5000)
+    }
   }
 
   return (
     <div>
+      {/* <Notification message={errorMessage} */}
       <h2>log in to application</h2>
       <form onSubmit={handleLogin}>
         <div>
