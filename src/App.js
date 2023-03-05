e06175023863c8ce8b68a210aee0de72c6023958
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-05 14:14:32 -0800

    save user to browser local storage on login

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBloglistUser', JSON.stringify(user)
+      )
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
