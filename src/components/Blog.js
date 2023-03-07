099c18691c3fd2fce8c11cdda796d17f79b58f29
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-06 19:40:32 -0800

    render each blog with button to toggle between showing and hiding details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,32 @@
-const Blog = ({blog}) => (
+import { useState } from 'react'
 
-  <div className="blogStyle">
-    <div>
-      {blog.title} {blog.author}
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className="blogStyle">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility} style={hideWhenVisible}>view</button>
+        <button onClick={toggleVisibility} style={showWhenVisible}>hide</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br />
+        likes {blog.likes}
+        <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Blog
