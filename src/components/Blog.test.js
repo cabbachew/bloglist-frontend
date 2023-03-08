7fedb644181700260c6f8d407be988ff56121938
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-03-07 17:59:54 -0800

    write and pass test for clicking to view blog details

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from "react"
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from "@testing-library/react"
+import { waitFor, render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Blog from "./Blog"
 
 test ("renders content", () => {
@@ -20,8 +21,8 @@ test ("renders content", () => {
     name: "Test user"
   }
 
-  const removeBlog = () => {}
-  const updatedBlog = () => {}
+  const removeBlog = jest.fn()
+  const updatedBlog = jest.fn()
 
   render (
     <Blog
@@ -31,7 +32,7 @@ test ("renders content", () => {
       currentUser={currentUser}
     />
   )
-  
+
   const element = screen.getByText("Test title Test author")
   // screen.debug(element)
   expect(element).toBeDefined()
@@ -39,4 +40,42 @@ test ("renders content", () => {
   // Alternative: const { container } = render(<Blog ... />)
   // const element = container.querySelector(".blog")
   // expect(element).toHaveTextContent("Test title Test author")
+})
+
+test ("clicking the button shows details", async () => {
+  const blog = {
+    title: "Test title",
+    author: "Test author",
+    url: "Test url",
+    likes: 0,
+    user: {
+      username: "Test username",
+      name: "Test user"
+    }
+  }
+
+  const currentUser = {
+    username: "Test username",
+    name: "Test user"
+  }
+
+  const removeBlog = jest.fn()
+  const updatedBlog = jest.fn()
+
+  render (
+    <Blog
+      blog={blog}
+      removeBlog={removeBlog}
+      updateBlog={updatedBlog}
+      currentUser={currentUser}
+    />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText("view")
+  await user.click(button)
+
+  // findByX variants should be used when the element is not immediately available
+  expect(await screen.findByText(/Test url/)).toBeDefined()
+  expect(await screen.findByText(/likes 0/)).toBeDefined()
 })
